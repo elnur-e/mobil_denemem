e11a55c02a6922025a016197d88650e21a28b319
elnur-e <elnureee0526@gmail.com>
2024-05-28 19:16:40 +0300

    first commit

---
+++ lib/utils/EAColors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+const primaryColor1 = Color(0xFFED3269);
+const primaryColor2 = Color(0xFFFD5F3E);
+const facebook = Color(0xFF3B5997);
+const twitter = Color(0xFF1DA0F1);
+
+const appColorPrimary = Color(0xFF1157FA);
+const iconColorPrimary = Color(0xFFFFFFFF);
+const iconColorSecondary = Color(0xFFA8ABAD);
+const appSecondaryBackgroundColor = Color(0xFF131d25);
+const appTextColorPrimary = Color(0xFF212121);
+const appTextColorSecondary = Color(0xFF5A5C5E);
+const appShadowColor = Color(0x95E9EBF0);
+const appColorPrimaryLight = Color(0xFFF9FAFF);
+
+// Dark Theme Colors
+const appBackgroundColorDark = Color(0xFF121212);
+const cardBackgroundBlackDark = Color(0xFF1F1F1F);
+const color_primary_black = Color(0xFF131d25);
+const iconColorPrimaryDark = Color(0xFF212121);
+const iconColorSecondaryDark = Color(0xFFA8ABAD);
+const appShadowColorDark = Color(0x1A3E3942);
